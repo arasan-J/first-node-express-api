573135ad7cabc9ea09fbdbc299969f48f0803dd7
Arasan J <arasanj7@gmail.com>
2020-11-30 18:38:40 +0530

    Added a conditio to differentiate user & developer.

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.json("welcome to API server!");
-   
+    if(req.headers["user-agent"].toLowerCase().indexOf("postman") > -1){
+        resp = "Hello Developer"
+    }
+    res.json(resp);
 });
 
 app.listen(port, () => {
