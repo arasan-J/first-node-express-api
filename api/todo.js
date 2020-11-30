cfe70f0c4ca9aea272f8d3c0d266812bbe68d5e3
Arasan J <arasanj7@gmail.com>
2020-11-30 20:27:06 +0530

    Getting one single todo item.

--- api/todo.js
+++ api/todo.js
@@ -6,5 +6,24 @@ let toDos = [];
 app.get("/", (req, res) => {
     res.json(toDos);
 } ); 
+app.get("/:toDoId", (req, res) => {
+    const { toDoId } = req.params;
+    if (toDos[+toDoId]) {
+        res.json(toDos[+toDoId]);
+} else {
+    res.status(404).json("item not found!");
+}
+});
+
+app.post("/", express.json(), (req, res) => {
+    if(typeof req.body.text !== "undefined"){
+        toDos.push(req.body.text);
+        res.json(toDos.length - 1);
+    } else {
+        res.status(400).json("You have to enter text.");
+    }
+});
+
+
 
 module.exports = app;
