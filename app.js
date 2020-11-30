3d172dda8dccc741b6225e93cfdff02978445431
Arasan J <arasanj7@gmail.com>
2020-11-30 13:30:14 +0530

    Started running a web server.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello everyone");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () =>{
+    console.log("Sever started on port $(port).");
+});
