4bb0f46df10b65eeaa1efcf0e420e47a15578d30
Arasan J <arasanj7@gmail.com>
2020-11-30 21:07:18 +0530

    Added new status codes for PUT and POST request

--- api/todo.js
+++ api/todo.js
@@ -11,25 +11,25 @@ app.get("/:toDoId", (req, res) => {
     if (toDos[+toDoId]) {
         res.json(toDos[+toDoId]);
 } else {
-    res.status(404).json("item not found!");
+    res.status(404).json("todo item not found!");
 }
 });
 
 app.post("/", express.json(), (req, res) => {
     if(typeof req.body.text !== "undefined"){
         toDos.push(req.body.text);
-        res.json(toDos.length - 1);
+        res.status(201).json(toDos.length - 1);
     } else {
         res.status(400).json("You have to enter text.");
     }
 });
 
-app.put("./:toDoId", express.json(), (req, res) => {
+app.put("/:toDoId", express.json(), (req, res) => {
     const { toDoId } = req.params;
     if (toDos[+toDoId]) {
         if(typeof req.body.text !== "undefined") {
         toDos[+toDoId] = req.body.text;
-        res.json(toDos[+toDoId]);
+        res.status(202).json(toDos[+toDoId]);
 }  else {
     res.status(400).json("You have to enter text.");
 } }
