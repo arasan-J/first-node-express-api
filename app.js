1571cc70eb5e9755b337a717bdea4827d59d2dc7
Arasan J <arasanj7@gmail.com>
2020-11-30 19:44:24 +0530

    Created a modular route for API

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 const morgan = require("morgan");
 const CORS = require("./cors");
 const port = 3000;
+const api = require("./api/root");
 
 app.use(morgan("combined"));
-
 app.use(CORS);
+ 
+app.use("/api", api);
+
 app.get("/", (req, res) => {
     res.json("welcome to API server!");
     if(req.headers["user-agent"].toLowerCase().indexOf("postman") > -1){
