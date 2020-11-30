4289101378a4f2c46170fd0d029be7d955a4b60a
Arasan J <arasanj7@gmail.com>
2020-11-30 19:55:35 +0530

    Initialised a data of toDos and implemented GET request.

--- api/todo.js
+++ api/todo.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express.Router();
 
+let toDos = [];
+
 app.get("/", (req, res) => {
-    res.json("This is in /api/todo");
+    res.json(toDos);
 } ); 
 
 module.exports = app;
